606e5a3e7b59ba00d9d49acef088adb912ed5473
Audio-Ninja <123126772+Audio-Ninja@users.noreply.github.com>
2024-06-30 15:39:20 -0600

    Help page

--- Main.java
+++ Main.java
@@ -10,9 +10,10 @@ public class Main {
         if(response.toLowerCase().equals("yes")) {
             clearScreen();
             System.out.println("Here is how you play:");
+            helpScreen();
         } else if (response.toLowerCase().equals("no")) {
             clearScreen();
-            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings.\nWalking by in front of you, you see a " + 
+            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings.\nOver on your right, you see a " + 
             mark("chicken") + "... with four legs? What is this place?");
             takeAction(0);
         } else {
@@ -37,12 +38,17 @@ public class Main {
             scan.close();
             return;
         }
+        if(action.toLowerCase().equals("help")) {
+            helpScreen();
+            scan.close();
+            return;
+        }
         if(action.length() > 7 && action.substring(0, 8).toLowerCase().equals("talk to ")) {
             String printText;
             String object = action.substring(8, action.length());
             printText = "Unfortunatly, you don't know how to talk to the " + object;
             if(object.toLowerCase().equals("chicken")) {
-                printText = "The weird " + mark("chicken") + " looks at you and says, \"Bawk baaaawk cluck cluck!\"";
+                printText = "The strange " + mark("chicken") + " turns to you and says, \"Oh! Hello there, traveler from the outer realm!\"";
             }
             System.out.println(printText);
             takeAction(0);
@@ -55,6 +61,16 @@ public class Main {
 
     public static String mark(String text) {
         return "\u001B[96m" + text + "\u001B[0m";
+    }
+
+    static void helpScreen() {
+        System.out.println("You play the game by entering actions based on your situation. The actions you can take are:");
+        System.out.println("talk to, look at, walk to, pick up, push, give, use, and use item on.\nObjects or entities you can interact with are " +
+        mark("highlighted") + ". \nFor example, you are in a place with a homeless " + mark("man") + " sitting by a big " + mark("stick") +
+        ". There is also a " + mark("path") + " leading off from your left. You have an " + mark("axe") + " and a loaf of " +
+        mark("bread") + " in your inventory.\nIn this situation, here are some actions you could type:\ntalk to man\nwalk to path\npush man" +
+        "\ngive bread to man\nuse bread\nuse axe on stick\n\nYou can also type " + mark("quests") + " to see your current and completed quests, " +
+        mark("i") + " to view your inventory, " + mark("end game") + " to end the game, and " + mark("help") + " to view this how to play section again.");
     }
 
     public static void main(String[] args) {
