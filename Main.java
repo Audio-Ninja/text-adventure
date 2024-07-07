6cfb2c9b1931dd75f90f1057f5f5914779f292f9
Audio-Ninja <123126772+Audio-Ninja@users.noreply.github.com>
2024-07-07 15:09:07 -0600

    More dialogue and such

--- Main.java
+++ Main.java
@@ -10,14 +10,14 @@ public class Main {
 
         if(response.toLowerCase().equals("yes")) {
             clearScreen();
-            System.out.println("Here is how you play:");
             helpScreen();
             goFromHelp();
         } else if (response.toLowerCase().equals("no")) {
             clearScreen();
-            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings.\nOver on your right, you see a " + 
-            mark("chicken") + "... with four legs? What is this place?");
-            takeAction(0);
+            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings. You find yourself " + 
+            "on a dirt path amidst a grassy plain. Ahead of you, the path forks away in multiple directions. \nOver on your right, you see a " + 
+            mark("rooster") + "... with four legs? What is this place?");
+            takeAction();
         } else {
             System.out.println("Invalid response. Try again.");
             gameStart();
@@ -30,7 +30,7 @@ public class Main {
         System.out.flush();
     }
 
-    static void takeAction(int place) {
+    static void takeAction() {
         Scanner scan = new Scanner(System.in);
         String action;
         int endIndex = 8;
@@ -46,31 +46,47 @@ public class Main {
             return;
         }
         if(action.toLowerCase().equals("help")) {
+            System.out.println();
             helpScreen();
             otherAction = true;
         }
-        if(action.toLowerCase().equals("lol")) {
-            if((int) (Math.random() * 2) == 1) {
-                lists.inventory.add("a stick");
-            } else {
-                lists.inventory.add("a sandwich");
-            }
-            otherAction = true;
-        }
-        if(action.toLowerCase().equals("bob")) {
-            lists.inventory.add("a block of cheese");
+        if(action.toLowerCase().equals("chez")) {
+            player.inventory.add("a block of " + mark("cheese"));
             otherAction = true;
         }
         if(action.toLowerCase().equals("i")) {
-            if(lists.inventory.size() == 0) {
+            if(player.inventory.size() == 0) {
                 System.out.println("You have nothing in your inventory.");
             } else {
                 String items = "You have ";
-                for(int i = 0; i < lists.inventory.size() - 1; i++) {
-                    items += lists.inventory.get(i) + ", ";
+                for(int i = 0; i < player.inventory.size() - 1; i++) {
+                    items += player.inventory.get(i) + ", ";
                 }
-                items += "and " + lists.inventory.get(lists.inventory.size() - 1) + ".";
+                items += "and " + player.inventory.get(player.inventory.size() - 1) + ".";
                 System.out.println(items);    
+            }
+            otherAction = true;
+        }
+        if(action.toLowerCase().equals("quests")) {
+            if(player.quests.size() == 0) {
+                System.out.println("You have no current quests.");
+            } else {
+                String quests = "Your current quests: ";
+                for(int i = 0; i < player.quests.size() - 1; i++) {
+                    quests += player.quests.get(i) + ", ";
+                }
+                quests += "and " + player.quests.get(player.quests.size() - 1) + ".";
+                System.out.println(quests);    
+            }
+            if(player.completedQuests.size() == 0) {
+                System.out.println("You have not completed any quests.");
+            } else {
+                String quests = "Your completed quests: ";
+                for(int i = 0; i < player.completedQuests.size() - 1; i++) {
+                    quests += player.completedQuests.get(i) + ", ";
+                }
+                quests += "and " + player.completedQuests.get(player.completedQuests.size() - 1) + ".";
+                System.out.println(quests);    
             }
             otherAction = true;
         }
@@ -79,8 +95,11 @@ public class Main {
                 String printText;
                 String object = action.substring(8, action.length());
                 printText = "Talk to... who?";
-                if(object.toLowerCase().equals("chicken")) {
-                    printText = "The strange " + mark("chicken") + " turns to you and says, \"Oh! Hello there, traveler from the outer realm!\"";
+                if(object.toLowerCase().equals("rooster")) {
+                    printText = "The strange " + mark("rooster") + " turns to you and cocks his head. \"Oh! Hello there, can I help you?\"\n" +
+                    "\"Where am I?\" You ask. \"How do I get back home?\"\nThe rooster's expression shifts to what you think is confusion. \"Uh... " + 
+                    "I didn't expect that question already... but the sorcerer should be able to send you back to your realm.\" He points a wing to where\n" + 
+                    "the path branches off toward some " + mark("mountains") + " to your left.\n\"He lives up there. And don't worry, I've heard he's a good fellow.\"";
                 }
                 System.out.println(printText);
             } else {
@@ -88,7 +107,7 @@ public class Main {
             }    
         }
         
-        takeAction(0);
+        takeAction();
         scan.close();
     }
 
@@ -103,26 +122,30 @@ public class Main {
         ". There is also a " + mark("path") + " leading off from your left. You have an " + mark("axe") + " and a loaf of " +
         mark("bread") + " in your inventory.\nIn this situation, here are some actions you could type:\ntalk to man\nwalk to path\npush man" +
         "\ngive bread to man\nuse bread\nuse axe on stick\n\nYou can also type " + mark("quests") + " to see your current and completed quests, " +
-        mark("i") + " to view your inventory, " + mark("end game") + " to end the game, and " + mark("help") + " to view this how to play section again.");
+        mark("i") + " to view your inventory, " + mark("end game") + " to end the game, and " + mark("help") + " to view this how to play section again.\n");
     }
 
     static void goFromHelp() {
-        System.out.println("\nType 'ready' when you are ready to proceed.");
+        System.out.println("Type 'ready' when you are ready to proceed.");
         Scanner scan = new Scanner(System.in);
         String response = scan.nextLine();
         if(response.toLowerCase().equals("ready")) {
             clearScreen();
-            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings.\nOver on your right, you see a " + 
-            mark("chicken") + "... with four legs? What is this place?");
-            takeAction(0);
+            System.out.println("Darkness.\nYour vision slowly clears, brightening until you can observe your surroundings. You find yourself " + 
+            "on a dirt path amidst a grassy plain. Ahead of you, the path forks away in multiple directions. \nOver on your right, you see a " + 
+            mark("rooster") + "... with four legs? What is this place?");
+            takeAction();
         } else {
             goFromHelp();
         }
         scan.close();
     }
 
-    public class lists {
+    public class player {
+        public static String place = "Grassy Plain";
         public static ArrayList<String> inventory = new ArrayList<String>();
+        public static ArrayList<String> quests = new ArrayList<String>();
+        public static ArrayList<String> completedQuests = new ArrayList<String>();
     }
 
     public static void main(String[] args) {
